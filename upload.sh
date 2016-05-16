9cf29f1cb6b2fe75d85f5d06d3a383ac86aabe79
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-05-16 22:46:11 +0100

    rm avrdude.conf

--- upload.sh
+++ upload.sh
@@ -1,2 +1,2 @@
 ./build.sh && \
-avrdude -C avrdude.conf -v -p atmega328p -c arduino -P /dev/ttyUSB0 -b 57600 -D -U flash:w:kern.hex:i
+avrdude -v -p atmega328p -c arduino -P /dev/ttyUSB0 -b 57600 -D -U flash:w:kern.hex:i
