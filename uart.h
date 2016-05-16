9f874cd542259c9e1abaa1a9b342c331a6544d4a
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-05-16 21:46:44 +0100

    docs

--- uart.h
+++ uart.h
@@ -3,8 +3,17 @@
 
 #include <stdint.h>
 
+// uart_init initializes the UART.
+// Baud rate is controlled by the macros:
+// 	BAUD, BAUD_TOL, USE_2X
+// 	UART_PORT_DDR, UART_RX_PIN, UART_TX_PIN
+// Protocol is 8 bits, no parity, 1 stop bit.
 void uart_init();
 
+// uart_put_sync writes x to the UART.
+// Synchronous write: waits for previous byte to be flushed.
+// Does not use interrupts.
+// uart_init must be called first.
 void uart_put_sync(uint8_t x);
 
 #endif
