d67fb64633d80148077e68b606384688a9ba2f8b
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-05-19 23:40:37 +0100

    comments

--- cattty/main.go
+++ cattty/main.go
@@ -1,49 +1,65 @@
+// catty redirects stdin/stdout to a TTY device. Usage:
+// 	catty -baud 9600 /dev/ttyUSB0
 package main
 
 import (
+	"flag"
+	"io"
 	"log"
 	"os"
-	"io"
 )
 
 // #include "ttycfg.h"
 import "C"
 
-const (
-	B50     = C.B50
-	B75     = C.B75
-	B110    = C.B110
-	B134    = C.B134
-	B150    = C.B150
-	B200    = C.B200
-	B300    = C.B300
-	B600    = C.B600
-	B1200   = C.B1200
-	B1800   = C.B1800
-	B2400   = C.B2400
-	B4800   = C.B4800
-	B9600   = C.B9600
-	B19200  = C.B19200
-	B38400  = C.B38400
-	B57600  = C.B57600
-	B115200 = C.B115200
-	B230400 = C.B230400
-)
+var baudCodes = map[int]int{
+	50:     C.B50,
+	75:     C.B75,
+	110:    C.B110,
+	134:    C.B134,
+	150:    C.B150,
+	200:    C.B200,
+	300:    C.B300,
+	600:    C.B600,
+	1200:   C.B1200,
+	1800:   C.B1800,
+	2400:   C.B2400,
+	4800:   C.B4800,
+	9600:   C.B9600,
+	19200:  C.B19200,
+	38400:  C.B38400,
+	57600:  C.B57600,
+	115200: C.B115200,
+	230400: C.B230400,
+}
+
+var baud = flag.Int("baud", 57600, "Baud rate")
 
 func main() {
-		f, err := os.OpenFile("/dev/ttyUSB0", os.O_RDWR|os.O_SYNC, 0666) // os.O_NOCTTY|
+	log.SetFlags(0)
+	flag.Parse()
+
+	if flag.NArg() != 1 {
+		log.Fatal("Need one file")
+	}
+
+	baudCode := baudCodes[*baud]
+	if baudCode == 0 {
+		log.Fatal("Invalid baud rate:", *baud)
+	}
+	f, err := os.OpenFile(flag.Arg(0), os.O_RDWR|os.O_SYNC, 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
 
-	_, err = C.initTTY(C.int(f.Fd()), B57600)
+	_, err = C.initTTY(C.int(f.Fd()), C.int(baudCode))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	go func(){log.Fatal(io.Copy(f, os.Stdin))}()
-	go func(){log.Fatal(io.Copy(os.Stdout, f))}()
+	go func() { log.Fatal(io.Copy(f, os.Stdin)) }()
+	go func() { log.Fatal(io.Copy(os.Stdout, f)) }()
 
-	select{}
+	select {}
 }
