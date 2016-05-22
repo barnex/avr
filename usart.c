fa5d72522b9fb12e98fda7af14c0effa2f91b631
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-05-22 22:02:39 +0100

    cleanup usart

--- usart.c
+++ usart.c
@@ -12,9 +12,9 @@
 #undef  USE_2X
 #include <util/setbaud.h>
 
-#define UART_PORT_DDR DDRD
-#define UART_RX_PIN   PIN0
-#define UART_TX_PIN   PIN1
+#define USART_PORT_DDR DDRD
+#define USART_RX_PIN   PIN0
+#define USART_TX_PIN   PIN1
 
 
 // for asynchronous writing
@@ -34,8 +34,8 @@ void usart_init() {
 	UCSR0A &= ~(_BV(U2X0));
 #endif
 
-	UART_PORT_DDR |= _BV(UART_TX_PIN);  // TX: output
-	UART_PORT_DDR &= ~_BV(UART_RX_PIN); // RX: input
+	USART_PORT_DDR |= _BV(USART_TX_PIN);  // TX: output
+	USART_PORT_DDR &= ~_BV(USART_RX_PIN); // RX: input
 	UCSR0B = _BV(RXEN0) | _BV(TXEN0);   // enable RX, TX
 	UCSR0C = _BV(UCSZ01) | _BV(UCSZ00); // 8-bit
 }
