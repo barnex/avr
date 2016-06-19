7ea5d0d980117bc8b672daca4524dc3453fd5695
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-06-19 13:58:01 +0100

    cleanup

--- spi_master.c
+++ spi_master.c
@@ -9,36 +9,17 @@
 #include <avr/interrupt.h>
 #include <avr/io.h>
 
-//static volatile int dump;
-
 int main() {
 
 	led_on();
 	usart_init();
 	debugln("\nUSART initialized");
 
-	//spi_init_slave();
-	//debugln("SPI slave");
-
 	spi_init_master();
 	debugln("SPI master");
 
-	//debugln("enabling interrupts");
-	//sei();
-
-
 	for(int i=0; i<5; i++) {
-
 		debugx(spi_tx(42)); debugln(" spi_tx");
-		//debugx(SPSR); debugln(" SPSR.");
-		//SPDR = 'A';
-		//debugx(SPSR); debugln(" SPSR.");
-
-		//debugln("delay");
-		//_delay_ms(1000);
-
-		//SPSR &= ~_BV(SPIF);
-
 	}
 
 	for (;;){}
